a1b72f11e33f506455b5a9d97bee01998fcd8a1e
hassy008 <hasnathrumman1234@gmail.com>
2019-02-01 00:58:08 +0600

    Custom 404 page with layout

---
+++ resources/views/errors/404.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <title>laraEbiz Error Page</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+  <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script> 
+  <!-- font files --> 
+  <link href="//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet" /> 
+  <link href="//fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet" /> 
+  <!-- /font files --> 
+  <!-- css files --> 
+  <link href="http://localhost/laraebiz/public/error/css/style.css" rel="stylesheet" type="text/css" media="all" /> 
+  <!-- /css files --> 
+  <!-- js files --> 
+  <!-- /js files --> 
+ </head>
+ <body> 
+  <div class="container demo-2"> 
+   <div class="content"> 
+    <div id="large-header" class="large-header"> 
+     <h1 class="header-w3ls">Booldy Error Page</h1> 
+     <p class="w3-agileits1">Oops!</p> 
+     <canvas id="demo-canvas"></canvas> 
+     <img src="http://localhost/laraebiz/public/error/images/owl.gif" alt="flashy" class="w3l" /> 
+     <h2 class="main-title">404</h2> 
+     <p class="w3-agileits2">We can't seem to find the page you're looking for.<br><br>
+      <a href="http://localhost/laraebiz" style="color:white; margin-left:135px; border:1px solid white;">Go Back To Home</a> 
+    </p> 
+    <br><br>
+     <p class="copyright">&copy; <script type="text/javascript">
+  document.write(new Date().getFullYear());
+</script> laraEbiz Error Page. All Rights Reserved | Design by <a href="https://localhost/laraebiz" target="_blank">laraEbiz</a></p> 
+    </div> 
+   </div> 
+  </div> 
+  <!-- js files --> 
+  <script src="http://localhost/laraebiz/public/error/js/rAF.js"></script>
+  <script src="http://localhost/laraebiz/public/error/js/demo-2.js"></script> 
+  <!-- /js files -->  
+ </body>
+</html>
